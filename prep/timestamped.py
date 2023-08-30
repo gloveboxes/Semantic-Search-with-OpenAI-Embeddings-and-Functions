6780da3d018f730babb8c7cee833197ad655b109
Dave Glover <dglover@microsoft.com>
2023-08-30 11:56:24 +1000

    single pass parse and overlap

--- prep/timestamped.py
+++ prep/timestamped.py
@@ -9,40 +9,10 @@ import os
 import json
 import csv
 import re
-# from langchain.text_splitter import TokenTextSplitter
 
-# allow for a 15% overlap between chunks
-# https://platform.openai.com/docs/api-reference/embeddings/create
-# text_splitter = TokenTextSplitter(chunk_size=8191, chunk_overlap=1200)
 
 segments = []
-SEGMENT_MIN_LENGTH = 10
-
-
-def gen_overlapping_segments():
-    current_video_id = None
-    # create a loop to iterate over the segments with x as the index
-    segment_count = 0
-    segments_len = len(segments)
-
-    for x in range(segments_len):
-        if current_video_id is None:
-            current_video_id = segments[x]['videoId']
-            # set segment_count to the number of segments with the same videoId
-            segment_count = len(
-                [segment for segment in segments if segment['videoId'] == current_video_id])
-
-        if current_video_id == segments[x]['videoId'] and segment_count > 1:
-            segment_count -= 1
-            words = segments[x + 1]['text'].split(' ')
-            word_count = len(words)
-            if word_count > 0:
-                # add the first 15% of the words to the current segment
-                append_text = ' '.join(words[0: int(word_count * 0.15)])
-                # print(append_text)
-                segments[x]['text'] += append_text
-        else:
-            current_video_id = None
+SEGMENT_MIN_LENGTH = 5
 
 
 def parse_vtt_transcript(vtt, segment):
@@ -51,6 +21,7 @@ def parse_vtt_transcript(vtt, segment):
     text = ""
     segment_begin_time = None
     segment_finish_time = None
+    segment_count = 0
 
     # open the vtt file
     with open(vtt, 'r', encoding='utf-8') as f:
@@ -68,7 +39,11 @@ def parse_vtt_transcript(vtt, segment):
             if re.match(r'^\d', line):
 
                 time_stamps = line.split(' --> ')
-                current_time = datetime.strptime(time_stamps[0], "%H:%M:%S.%f")
+
+                try:
+                    current_time = datetime.strptime(time_stamps[0], "%H:%M:%S.%f")
+                except ValueError:
+                    continue
 
                 if segment_begin_time is None:
                     # get the time stamps
@@ -96,6 +71,16 @@ def parse_vtt_transcript(vtt, segment):
                 # add the text to the transcript
                 text += line
             else:
+                # add 15% of the text to the previous segment
+                if segment_count > 0:
+                    # add 15% of the text to the previous segment
+                    words = text.split(' ')
+                    word_count = len(words)
+                    if word_count > 0:
+                        append_text = ' '.join(words[0: int(word_count * 0.15)])
+                        segments[-1]['text'] += append_text
+
+                segment_count += 1
                 segment['start'] = segment_begin_time.strftime('%H:%M:%S')
                 segment['text'] = text
                 segments.append(segment.copy())
@@ -106,9 +91,7 @@ def parse_vtt_transcript(vtt, segment):
 
     # add the last text segment segments dictionary
     if segment_begin_time is not None and text != "":
-        segment['start'] = segment_begin_time.strftime('%H:%M:%S')
-        segment['text'] = text
-        segments.append(segment.copy())
+        segments[-1]['text'] += text
 
 
 def get_transcript(meta):
@@ -139,11 +122,11 @@ def get_transcript(meta):
 for file in glob.glob("./prep/transcripts/*.json"):
 
     # load the json file
-    meta = json.load(open(file))
+    meta = json.load(open(file, encoding='utf-8'))
 
     get_transcript(meta)
 
-gen_overlapping_segments()
+# gen_overlapping_segments()
 
 # # write the sessions to a csv file
 with open('master.csv', 'w', newline='', encoding='utf-8') as csvfile:
