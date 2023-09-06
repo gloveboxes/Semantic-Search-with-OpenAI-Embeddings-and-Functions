8c77745f59eee9ba40e6f0835dc5dec185231902
Dave Glover <dglover@microsoft.com>
2023-09-06 15:29:07 +1000

    pwsh pipeline

--- prep/bucket_transcriptions.py
+++ prep/bucket_transcriptions.py
@@ -8,6 +8,7 @@ import glob
 import os
 import json
 import re
+import sys
 
 
 segments = []
---
+++ prep/prepare_transcripts.ps1
@@ -0,0 +1,8 @@
+# This script will process the transcripts
+# First it will bucket up all the transcripts into 5 minute segments
+# Next it will call OpenAI ChatGPT to summarize each 5 minute segment
+# Finally, it will create embeddings for each 5 minute segment
+
+python3 bucket_transcriptions.py
+python3 openai_summarize_transcripts.py
+python3 openai_create_embeddings.py
